0d6ec53557460e0316c50dae35d1c0abfa9d78f5
Cherise <cherisetan@live.com>
2019-05-15 16:28:26 +1200

    Allow the user only 3 guesses

--- number_guesser.rb
+++ number_guesser.rb
@@ -2,14 +2,29 @@
 # answer = rand(1..10)
 answer = 1
 
-# Asks user to guess a number
-puts "Guess a number between 1 and 10."
+# Initialises number of turns as zero
+turnCount = 0
 
-# Stores the user number in a variable, and converts it to an integer
-guess = gets.chomp.to_i
+loop do
+  # Asks user to guess a number
+  puts "Guess a number between 1 and 10."
 
-if answer == guess
-  puts "You guessed correctly"
-else
-  puts "You guessed wrong"
+  # Stores the user number in a variable, and converts it to an integer
+  guess = gets.chomp.to_i
+
+  # Tell the user if they guessed correctly
+  if answer == guess
+    puts "You guessed correctly"
+    break
+  else
+    puts "You guessed wrong"
+    # Increment the number of turns
+    turnCount += 1
+
+    # Checks if the users has had 3 guesses
+    if turnCount == 3
+      puts "You lost"
+      break # Breaks the loop if the user has had three guesses
+    end
+  end
 end

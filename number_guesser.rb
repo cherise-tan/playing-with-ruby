002fac641586faa8d8625ed46dec8c79fe7370b3
Cherise <cherisetan@live.com>
2019-05-15 15:15:45 +1200

    Stores the user number in a variable, and converts it to an integer

--- number_guesser.rb
+++ number_guesser.rb
@@ -3,3 +3,8 @@ answer = rand(1..10)
 
 # Asks user to guess a number
 puts "Guess a number between 1 and 10."
+
+# Stores the user number in a variable, and converts it to an integer
+guess = gets.chomp.to_i
+
+puts guess
